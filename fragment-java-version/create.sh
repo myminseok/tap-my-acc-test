e6fadaa3ab05ac4cdbe38a9c3ae0523b63f2dde0
myminseok <myminseok@gmail.com>
2022-11-22 13:42:00 +0900

    fragment

--- fragment-java-version/create.sh
+++ fragment-java-version/create.sh
@@ -4,3 +4,11 @@ tanzu accelerator fragment create tap-my-acc-fragment-java-version \
   --git-sub-path fragment-java-version
   
   tanzu accelerator fragment get tap-my-acc-fragment-java-version
+
+  ###
+  ## artifact:
+  ## message: stored artifact for revision 'main/26e9566617f16a0699f7da10181bbae1a1d3fab1'
+  ## ready: true
+
+
+  
