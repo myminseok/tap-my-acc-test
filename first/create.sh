34f4f21f4a192f78fb0a56e674eee254739de413
myminseok <myminseok@gmail.com>
2022-11-22 17:38:23 +0900

    update

--- first/create.sh
+++ first/create.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source ../env.yml
 tanzu accelerator create tap-my-cc-first --local-path .  --source-image $IMAGE_REGISTRY/tap-source/tap-my-cc-first --interval 10s
 tanzu accelerator get tap-my-cc-first
